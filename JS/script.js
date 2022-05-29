132fe4c9a196c18892285dc65bb805976ae1e4f6
HeroldKiraly <kiraly-h@protonmail.com>
2022-05-29 16:08:00 -0400

    Javascript update/Page Style

--- JS/script.js
+++ JS/script.js
@@ -1,45 +1,32 @@
 const possibleAnswers = [/*Positive*/ 'It is certain.', 'It is decidedly so.', 'Without a doubt.', 'Yes definitely.', 'You may rely on it.', 'As I see it, yes.', 'Most likely.', 'Outlook good.', 'Yes.', 'Signs point to a yes.', /*Neutral*/ 'Reply hazy, try again.', 'Ask again later.', 'Better not tell you now', 'Cannot predict now.', 'Concentrate and ask again.', /*Negative*/ 'Dont count on it.', 'My reply is no.', 'My sources say no.', 'Outlook not so good.', 'Very doubtful.'];
 
-let getGameOutput = possibleAnswers[Math.floor(Math.random() * possibleAnswers.length)];
+//GAME GLOBAL VARIABLES
 
 let randomDecider = Math.floor(Math.random() * 20);
+var document;
+//BUTTON VARIABLE
+let buttonClick = document.getElementById("button").onclick = answerOnButtonPress;
 
-
-function dataLog() {
-    console.log(possibleAnswers.length);
-    console.log(randomDecider);
+function answerOnButtonPress() {
+    //FUNCTION VARIABLES
+    let gameInput = document.getElementById('game-input');
+    let gameOutput = document.getElementById('data-game-output');
+    let getGameOutput = possibleAnswers[Math.floor(Math.random() * possibleAnswers.length)];
+    var userCurrentInput;
+    
+    if (gameInput.value == false) {
+        gameOutput.innerHTML = 'Please write something as an input';
+    } else if (userCurrentInput == gameInput.value) {
+        gameOutput.innerHTML = 'Please ask something else';
+    } else if(gameInput.value != false && userCurrentInput == null) {
+        gameOutput.innerHTML = getGameOutput;
+        userCurrentInput = gameInput.value;
+    }
 }
+buttonClick;
 
-document.getElementById('data-game-output').innerHTML = getGameOutput;
+console.log(userCurrentInput);
+
 
 dataLog();
 console.log(getGameOutput);
-
-
-
-
-/*
-● It is certain.
-● It is decidedly so.
-● Without a doubt.
-● Yes definitely.
-● You may rely on it.
-
-● As I see it, yes.
-● Most likely.
-● Outlook good.
-● Yes.
-● Signs point to yes.
-
-● Reply hazy, try again.
-● Ask again later.
-● Better not tell you now.
-● Cannot predict now.
-● Concentrate and ask again.
-
-● Don't count on it.
-● My reply is no.
-● My sources say no.
-● Outlook not so good.
-● Very doubtful.
-*/
