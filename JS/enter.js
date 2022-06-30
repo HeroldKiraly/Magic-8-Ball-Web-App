10cf50050e9997fb61577f937d03a7ff796dddd9
HeroldKiraly <kiraly-h@protonmail.com>
2022-06-30 15:51:11 -0400

    Updated enter key usage and working script

--- JS/enter.js
+++
@@ -1,8 +0,0 @@
-var input = document.getElementById("game-input");
-
-input.addEventListener("keypress", function(event) {
-    if (event.key === "Enter") {
-        event.preventDefault();
-        document.getElementById("button").click();
-    }
-});
--- JS/script.js
+++ JS/script.js
@@ -1,22 +1,23 @@
+//8 BALL ANSWERS ARRAY [POSITIVE, NEUTRAL, NEGATIVE]
 const possibleAnswers = [/*Positive*/ 'It is certain.', 'It is decidedly so.', 'Without a doubt.', 'Yes definitely.', 'You may rely on it.', 'As I see it, yes.', 'Most likely.', 'Outlook good.', 'Yes.', 'Signs point to a yes.', /*Neutral*/ 'Reply hazy, try again.', 'Ask again later.', 'Better not tell you now', 'Cannot predict now.', 'Concentrate and ask again.', /*Negative*/ 'Dont count on it.', 'My reply is no.', 'My sources say no.', 'Outlook not so good.', 'Very doubtful.'];
 
+
 //GAME GLOBAL VARIABLES
-
 let randomDecider = Math.floor(Math.random() * 20);
-var document;
-//BUTTON VARIABLE
+let document;
 let buttonClick = document.getElementById("button").onclick = answerOnButtonPress;
+let userCurrentInput;
 
+
+//GAME FUNCTION
 function answerOnButtonPress() {
     //FUNCTION VARIABLES
     let gameInput = document.getElementById('game-input');
     let gameOutput = document.getElementById('data-game-output');
     let getGameOutput = possibleAnswers[Math.floor(Math.random() * possibleAnswers.length)];
-    var userCurrentInput;
     
     if (gameInput.value == false) {
-        gameOutput.innerHTML = 'Please write something as an input';
-        userCurrentInput = gameInput.value;
+        gameOutput.innerHTML = 'Please write something as an input.';
     } else if (userCurrentInput === gameInput.value) {
         gameOutput.innerHTML = 'Please ask something else';
     } else if(gameInput.value != false && userCurrentInput == null) {
@@ -26,8 +27,15 @@ function answerOnButtonPress() {
 }
 buttonClick;
 
+
+//RUN FUNCTION ON ENTER KEY PRESS
+document.addEventListener("keydown", function(event) {
+    if (event.code === "Enter") {
+        answerOnButtonPress();
+    }
+});
+
+//LOGS
 console.log(userCurrentInput);
-
-
 dataLog();
 console.log(getGameOutput);
