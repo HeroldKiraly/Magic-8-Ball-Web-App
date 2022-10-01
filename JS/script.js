931e627c554031e2800a7aaeffb7eaf87615d967
HeroldKiraly <kiraly-h@protonmail.com>
2022-10-01 13:33:14 -0400

    Fixed bugs in game

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,10 @@
-//8 BALL ANSWERS ARRAY [POSITIVE, NEUTRAL, NEGATIVE]
+// 8 BALL ANSWERS ARRAY [POSITIVE, NEUTRAL, NEGATIVE]
 const possibleAnswers = [/*Positive*/ 'It is certain.', 'It is decidedly so.', 'Without a doubt.', 'Yes definitely.', 'You may rely on it.', 'As I see it, yes.', 'Most likely.', 'Outlook good.', 'Yes.', 'Signs point to a yes.', /*Neutral*/ 'Reply hazy, try again.', 'Ask again later.', 'Better not tell you now', 'Cannot predict now.', 'Concentrate and ask again.', /*Negative*/ 'Dont count on it.', 'My reply is no.', 'My sources say no.', 'Outlook not so good.', 'Very doubtful.'];
-
 
 //GAME GLOBAL VARIABLES
 let randomDecider = Math.floor(Math.random() * 20);
-let document;
 let buttonClick = document.getElementById("button").onclick = answerOnButtonPress;
 let userCurrentInput;
-
 
 //GAME FUNCTION
 function answerOnButtonPress() {
@@ -20,13 +17,12 @@ function answerOnButtonPress() {
         gameOutput.innerHTML = 'Please write something as an input.';
     } else if (userCurrentInput === gameInput.value) {
         gameOutput.innerHTML = 'Please ask something else';
-    } else if(gameInput.value != false && userCurrentInput == null) {
+    } else if (gameInput.value != false && userCurrentInput != gameInput.value) {
         gameOutput.innerHTML = getGameOutput;
         userCurrentInput = gameInput.value;
     }
 }
 buttonClick;
-
 
 //RUN FUNCTION ON ENTER KEY PRESS
 document.addEventListener("keydown", function(event) {
@@ -37,5 +33,3 @@ document.addEventListener("keydown", function(event) {
 
 //LOGS
 console.log(userCurrentInput);
-dataLog();
-console.log(getGameOutput);
